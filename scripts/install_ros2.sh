f1d0a2de80fdc56c366b8c9f25c374cdd9a21df2
Byungchan Kim <83104885+Bchan725@users.noreply.github.com>
2025-12-25 13:43:04 +0000

    deleted duplicated command

--- scripts/install_ros2.sh
+++ scripts/install_ros2.sh
@@ -47,8 +47,3 @@ echo "install pcl"
 sudo apt install libpcl-dev pcl-tools -y
 
 sudo apt install build-essential cmake -y
-
-echo "Auto activate ros2" 
-
-echo "source /opt/ros2/humble/setup.bash" >> ~/.bashrc
-source ~/.bashrc
